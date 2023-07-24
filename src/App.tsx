441b71864043cdfa5090b3f8607ae80cd74a616c
Daizaikun <laiglesias.min@gmail.com>
2023-07-24 13:14:47 -0500

    add router save

--- src/App.tsx
+++ src/App.tsx
@@ -2,31 +2,29 @@ import { Route } from "wouter";
 import { Home } from "./routers/home";
 import { User } from "./routers/user";
 
-import { Client, cacheExchange, fetchExchange , Provider } from 'urql';
+import { Client, Provider, cacheExchange, fetchExchange } from "urql";
+import { Save } from "./routers/save";
 
 const client = new Client({
-  url: 'http://localhost:4000/graphql',
-  exchanges: [cacheExchange, fetchExchange],
+	url: "http://localhost:8080/graphql",
+	exchanges: [cacheExchange, fetchExchange],
 });
 
 function App() {
-
-  
-
 	return (
 		<>
-    <Provider value={client}>
-			<div>
-				{/* <Link href="/users/1">Profile</Link> */}
-				<Route path="/" component={Home} />
+			<Provider value={client}>
+				<div>
+					{/* <Link href="/users/1">Profile</Link> */}
+					<Route path="/" component={Home} />
 
-				<Route path="/users/:name">
-					{(params) => <User name={params.name} />}
-				</Route>
+					<Route path="/users/:name">
+						{(params) => <User name={params.name} />}
+					</Route>
 
-				<Route path="/save" component={Home} />
-			</div>
-      </Provider>
+					<Route path="/save" component={Save} />
+				</div>
+			</Provider>
 		</>
 	);
 }
