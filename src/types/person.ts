00d59959e3694b9fa92d270d773e87f8a6c19920
Daizaikun <laiglesias.min@gmail.com>
2023-07-24 13:22:29 -0500

    update type person || add save type

--- src/types/person.ts
+++ src/types/person.ts
@@ -1,14 +1,14 @@
 export type person = {
-  id?: number;
-  login: string;
-  githubID: number;
-  avatarUrl: string;
-  apiUrl: string;
-  htmlUrl: string;
-  name: string;
-  location?: string;
-  publicRepos?: number;
-  followers?: number;
-  following?: number;
+	id?: number;
+	login: string;
+	githubID: number;
+	avatarUrl: string;
+	apiUrl: string;
+	htmlUrl: string;
+	name: string;
+	location?: string;
+	publicRepos?: number;
+	followers?: number;
+	following?: number;
+	save?: boolean;
 };
-
