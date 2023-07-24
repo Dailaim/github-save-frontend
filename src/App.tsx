1bf3a2c9b186c86ed2e9c5869bde3aa7965dc5c8
Daizaikun <laiglesias.min@gmail.com>
2023-07-23 20:57:19 -0500

    add urql

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,31 @@ import { Route } from "wouter";
 import { Home } from "./routers/home";
 import { User } from "./routers/user";
 
+import { Client, cacheExchange, fetchExchange , Provider } from 'urql';
+
+const client = new Client({
+  url: 'http://localhost:4000/graphql',
+  exchanges: [cacheExchange, fetchExchange],
+});
+
 function App() {
+
+  
+
 	return (
 		<>
+    <Provider value={client}>
 			<div>
 				{/* <Link href="/users/1">Profile</Link> */}
 				<Route path="/" component={Home} />
 
 				<Route path="/users/:name">
-					{(params) => <User id={params.name} />}
+					{(params) => <User name={params.name} />}
 				</Route>
+
+				<Route path="/save" component={Home} />
 			</div>
+      </Provider>
 		</>
 	);
 }
