b227af2fb13a6f8673928951cf84746932b980fd
Daizaikun <laiglesias.min@gmail.com>
2023-07-23 22:19:08 -0500

    fix link users

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -30,7 +30,7 @@ export const Card: FC<{ person: person; onSave?: () => void }> = ({
 				<div className="-mt-px flex divide-x divide-gray-200 dark:divide-gray-700">
 					<div className="flex w-0 flex-1">
 						<Link
-							href={`/users/${person.name}`}
+							href={`/users/${person.login}`}
 							className="relative -mr-px inline-flex w-0 flex-1 items-center justify-center gap-x-3 rounded-bl-lg border border-transparent py-4 text-sm font-semibold text-gray-900 dark:text-gray-200"
 						>
 							Ver perfil
