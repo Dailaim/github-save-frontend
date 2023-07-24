f9a3a9808b5e22de582148342536b1acb4082270
Daizaikun <laiglesias.min@gmail.com>
2023-07-23 22:16:50 -0500

    delete count

--- src/context/peopleListContext.ts
+++ src/context/peopleListContext.ts
@@ -4,14 +4,12 @@ import { person } from "../types/person";
 type Store = {
 	search: string;
 	people: person[];
-	count: number;
 	autoSearch: boolean;
 };
 
 interface Actions {
 	setSearch: (val: string) => void;
 	setPeople: (val: person[]) => void;
-	setCount: (val: number) => void;
 	setAutoSearch: (val: boolean) => void;
 }
 
@@ -19,7 +17,6 @@ const initialState = {
 	autoSearch: true,
 	search: "",
 	people: [],
-	count: 0,
 };
 
 export const usePeopleState = create<Store & Actions>((set) => ({
@@ -28,8 +25,6 @@ export const usePeopleState = create<Store & Actions>((set) => ({
 	setSearch: (val) => set({ search: val }),
 
 	setPeople: (val) => set({ people: val }),
-
-	setCount: (val) => set({ count: val }),
 
 	setAutoSearch: (val) => set({ autoSearch: val }),
 }));
